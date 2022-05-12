90b34695a9f2a8f722c3c8d723c2eecd320c7242
Andrew Wittrock <andy@wittrock.us>
2022-05-11 23:42:54 -0500

    Upgrade React to 18

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './components/App';
 import './index.scss';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const container = document.getElementById('root');
+const root = createRoot(container!);
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
