9bf8fc7366322c78291382c2456c99fc788178b9
Andrew Wittrock <andy@wittrock.us>
2022-10-07 06:29:50 -0500

    Upgrade dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { App } from './components/App';
 import './index.scss';
-import { createRoot } from 'react-dom/client';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
